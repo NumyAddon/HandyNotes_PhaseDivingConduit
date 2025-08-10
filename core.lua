62d8162b28ee7a4d8747bce97d6bb304d1cfa606
Mark W <numedain@gmail.com>
2025-08-10 19:04:13 +0200

    Initial release

---
+++ core.lua
@@ -0,0 +1,164 @@
+local addonName = ...;
+--- @class PhaseDivingConduit_NS
+local ns = select(2, ...);
+local PDC = LibStub('AceAddon-3.0'):NewAddon(addonName);
+
+local HandyNotes = LibStub('AceAddon-3.0'):GetAddon('HandyNotes', true);
+if not HandyNotes then return; end
+
+local HBD = LibStub('HereBeDragons-2.0');
+
+--@debug@
+_G.HN_PhaseDivingConduit = PDC;
+--@end-debug@
+
+local mapID = 2371; -- K'aresh
+local cityMapID = 2472; -- Tazavesh
+local atlas = 'flightmaster_ancientwaygate-taxinode_neutral';
+local atlasInfo = C_Texture.GetAtlasInfo(atlas);
+local iconDefault = {
+    icon = atlasInfo.file,
+    tCoordLeft = atlasInfo.leftTexCoord,
+    tCoordRight = atlasInfo.rightTexCoord,
+    tCoordTop = atlasInfo.topTexCoord,
+    tCoordBottom = atlasInfo.bottomTexCoord,
+};
+
+local locations = ns.locations;
+
+--- @class PhaseDivingConduit_Node
+--- @field x number
+--- @field y number
+--- @field name string
+--- @field taxiNodeID number
+
+--- @type table<number, PhaseDivingConduit_Node[]> # [uiMapID] = list of locations
+PDC.nodes = {
+    [mapID] = {},
+    [cityMapID] = {},
+};
+
+function PDC:OnInitialize()
+    for _, pin in ipairs(locations) do
+        local x, y = HBD:GetZoneCoordinatesFromWorld(pin.pos1, pin.pos0, mapID);
+        if x and y then
+            local coord = HandyNotes:getCoord(x, y);
+            --- @type PhaseDivingConduit_Node
+            self.nodes[mapID][coord] = {
+                name = pin.name,
+                taxiNodeID = pin.taxiNodeID,
+                x = x,
+                y = y,
+            };
+        end
+        x, y = HBD:GetZoneCoordinatesFromWorld(pin.pos1, pin.pos0, cityMapID);
+        if x and y then
+            local coord = HandyNotes:getCoord(x, y);
+            --- @type PhaseDivingConduit_Node
+            self.nodes[cityMapID][coord] = {
+                name = pin.name,
+                taxiNodeID = pin.taxiNodeID,
+                x = x,
+                y = y,
+            };
+        end
+    end
+
+    local defaults = {
+        profile = {
+            iconScale = 1.0,
+            iconAlpha = 1.0,
+        },
+    };
+    self.db = LibStub('AceDB-3.0'):New('HandyNotes_PhaseDivingConduitDB', defaults, true).profile;
+
+    if (C_AddOns.IsAddOnLoaded('TomTom')) then
+        self.isTomTomLoaded = true;
+    end
+    local increment = CreateCounter(1);
+    local options = {
+        type = 'group',
+        name = 'Phase Diving Conduit',
+        desc = 'Locations of Phase Diving Conduits on K\'aresh',
+        get = function(info) return self.db[info[#info]]; end,
+        set = function(info, v) self.db[info[#info]] = v; self:Refresh(); end,
+        args = {
+            desc = {
+                name = 'These settings control the look and feel of the icon.',
+                type = 'description',
+                order = increment(),
+            },
+            iconScale = {
+                type = 'range',
+                name = 'Icon Scale',
+                desc = 'The scale of the icons',
+                min = 0.25, max = 3, step = 0.01,
+                order = increment(),
+            },
+            iconAlpha = {
+                type = 'range',
+                name = 'Icon Alpha',
+                desc = 'The alpha transparency of the icons',
+                min = 0, max = 1, step = 0.01,
+                order = increment(),
+            },
+        },
+    };
+    HandyNotes:RegisterPluginDB(addonName, self, options);
+end
+
+local function iter(t, previousIndex)
+    if not t then return nil; end
+    local index, value = next(t, previousIndex);
+    while index and value do
+        return index, nil, iconDefault, PDC.db.iconScale * 1.2, PDC.db.iconAlpha * 2;
+    end
+end
+function PDC:GetNodes2(uiMapId, isMinimapUpdate)
+    if not self.nodes[uiMapId] or isMinimapUpdate then return function() end end
+
+    return iter, self.nodes[uiMapId], nil
+end
+
+function PDC.OnClick(mapPin, button, down, mapFile, coord)
+    if not down then return; end
+    local node = PDC.nodes[mapFile][coord];
+    if not node then return; end
+    if IsAltKeyDown() and PDC.isTomTomLoaded then
+        TomTom:AddWaypoint(mapFile, node.x, node.y, {
+            title = node.name,
+            from = addonName,
+            persistent = nil,
+            minimap = true,
+            world = true,
+        });
+    end
+end
+
+function PDC.OnEnter(mapPin, mapFile, coord)
+    local node = PDC.nodes[mapFile][coord];
+    if not node then return; end
+
+    if mapPin:GetCenter() > UIParent:GetCenter() then
+        GameTooltip:SetOwner(mapPin, 'ANCHOR_LEFT');
+    else
+        GameTooltip:SetOwner(mapPin, 'ANCHOR_RIGHT');
+    end
+
+    local text = node.name;
+    GameTooltip:SetText('Phase Conduit');
+    GameTooltip:AddLine(text);
+
+    if PDC.isTomTomLoaded then
+        GameTooltip_AddInstructionLine(GameTooltip, 'Alt-click to set TomTom waypoint');
+    end
+    GameTooltip:Show();
+end
+
+function PDC:OnLeave()
+    GameTooltip:Hide()
+end
+
+function PDC:Refresh()
+    HandyNotes:SendMessage('HandyNotes_NotifyUpdate', addonName);
+end
---
+++ data.lua
@@ -0,0 +1,28 @@
+--- @class PhaseDivingConduit_NS
+local ns = select(2, ...);
+
+--- @class PhaseDivingConduit_LocationInfo
+--- @field taxiNodeID number
+--- @field continentID number
+--- @field pos0 number
+--- @field pos1 number
+--- @field name string
+
+--- generated map of locations
+--- @type PhaseDivingConduit_LocationInfo[]
+ns.locations = {
+    { taxiNodeID = 3145, continentID = 2738, pos0 = -1206.65002441410, pos1 = -217.33900451660, name = "Tazavesh, the Veiled Market" },
+    { taxiNodeID = 3146, continentID = 2738, pos0 = 1391.81994628910, pos1 = -1040.18005371090, name = "The Oasis" },
+    { taxiNodeID = 3147, continentID = 2738, pos0 = 1611.43005371090, pos1 = 249.98599243164, name = "Shan'dorah" },
+    { taxiNodeID = 3155, continentID = 2738, pos0 = 1891.19995117190, pos1 = 1497.51000976560, name = "Shadow Point" },
+    { taxiNodeID = 3156, continentID = 2738, pos0 = 529.13201904297, pos1 = 999.41101074219, name = "Serrated Peaks" },
+    { taxiNodeID = 3157, continentID = 2738, pos0 = 1201.55004882810, pos1 = 1062.01000976560, name = "Overlook Zo'Shuul" },
+    { taxiNodeID = 3158, continentID = 2738, pos0 = 562.82098388672, pos1 = -170.86500549316, name = "Lunnall River" },
+    { taxiNodeID = 3159, continentID = 2738, pos0 = 509.88198852539, pos1 = -1143.69995117190, name = "North Sufaad" },
+    { taxiNodeID = 3160, continentID = 2738, pos0 = 25.08849906921, pos1 = 365.99499511719, name = "Naakroa" },
+    { taxiNodeID = 3161, continentID = 2738, pos0 = 2027.46997070310, pos1 = 596.68402099609, name = "Fracture of Laacuna" },
+    { taxiNodeID = 3162, continentID = 2738, pos0 = 1733.56005859380, pos1 = 771.22100830078, name = "Untethered Space" },
+    { taxiNodeID = 3163, continentID = 2738, pos0 = -482.05599975586, pos1 = 985.00897216797, name = "Ruins of Yaathron" },
+    { taxiNodeID = 3164, continentID = 2738, pos0 = -289.40499877930, pos1 = 787.75500488281, name = "Hosaas' Rest" },
+};
+
